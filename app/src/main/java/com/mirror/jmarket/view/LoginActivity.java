c2a9473b523514ff5d64e7ad655bd7b874c0630e
hyeonseongkang <gt9697@naver.com>
2022-09-05 17:35:28 +0900

    login activity

---
+++ app/src/main/java/com/mirror/jmarket/view/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.mirror.jmarket.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mirror.jmarket.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
