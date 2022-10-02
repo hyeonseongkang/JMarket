c590779087279c2abaf4e16a1c9585ba8bf32460
HyeonSeongKang <gt9697@naver.com>
2022-10-03 01:22:40 +0900

    내가 받은 리뷰, 내가 작성한 리뷰 fragment ReviewListActivity에 연결

--- app/src/main/java/com/mirror/jmarket/view/MyReviewListActivity.java
+++ app/src/main/java/com/mirror/jmarket/view/MyReviewListActivity.java
@@ -3,7 +3,9 @@ package com.mirror.jmarket.view;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.material.tabs.TabLayout;
 import com.mirror.jmarket.R;
 import com.mirror.jmarket.databinding.ActivityMyReviewListBinding;
 
@@ -23,5 +25,41 @@ public class MyReviewListActivity extends AppCompatActivity {
         // written review
 
         overridePendingTransition(R.anim.fadein_left, R.anim.none);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, new MyReceivedReviewListFragment()).commit();
+
+        binding.tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+
+                if (position == 0) {
+                    // 내가 받은 리뷰
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new MyReceivedReviewListFragment()).commit();
+                } else if (position == 1) {
+                    // 내가 작성한 리뷰
+                    getSupportFragmentManager().beginTransaction().replace(R.id.container, new MyWrittenReviewListFragment()).commit();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        // button
+        binding.backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                overridePendingTransition(R.anim.none, R.anim.fadeout_left);
+            }
+        });
     }
 }
